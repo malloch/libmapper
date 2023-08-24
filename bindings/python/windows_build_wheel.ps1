c3a33ba8a1ce4a953c665bcaf3fe9ff3d580fbd6
Joseph Malloch <joseph.malloch@gmail.com>
2023-08-24 00:02:52 -0300

    Python bindings: rearrange folder structure to match PyPI.

--- bindings/python/windows_build_wheel.ps1
+++ bindings/python/windows_build_wheel.ps1
@@ -2,17 +2,17 @@ cd $PSScriptRoot
 
 # Rename .py.in files to .py for Windows
 cp ./setup.py.in ./setup.py
-cp ./libmapper/mapper.py.in ./libmapper/mapper.py
+cp ./src/libmapper/mapper.py.in ./src/libmapper/mapper.py
 
 # Replace version env variable with actual version
 $versionString = git describe --tags
 $versionString = $versionString -replace '([0-9.]+)-([0-9]+)-([a-z0-9]+)', '$1.$2+$3'
-(gc ./libmapper/mapper.py) -replace '__version__ = ''@PACKAGE_VERSION@''', "__version__ = '$versionString'" | Out-File -encoding ASCII ./libmapper/mapper.py
+(gc ./src/libmapper/mapper.py) -replace '__version__ = ''@PACKAGE_VERSION@''', "__version__ = '$versionString'" | Out-File -encoding ASCII ./src/libmapper/mapper.py
 
 # Copy dlls to python bindings folder
-cp -v ../../dist/libmapper.dll ./libmapper/libmapper.dll
-cp -v ../../dist/liblo.dll ./libmapper/liblo.dll
-cp -v ../../dist/zlib.dll ./libmapper/zlib.dll
+cp -v ../../dist/libmapper.dll ./src/libmapper/libmapper.dll
+cp -v ../../dist/liblo.dll ./src/libmapper/liblo.dll
+cp -v ../../dist/zlib.dll ./src/libmapper/zlib.dll
 
 # Build python wheel
 pip install wheel
