4eedf65fc3a0ef98c2b1635296e3e484974ca171
Joseph Malloch <joseph.malloch@gmail.com>
2023-10-31 13:31:26 -0300

    CI: trying to run tests on Windows runner.

--- test/windows_tests.ps1
+++ test/windows_tests.ps1
@@ -3,7 +3,7 @@ Write-Host $pwd
 Get-ChildItem -Path $pwd\..\build\test\Release
 Get-ChildItem -Path $pwd\..\build\test\Release\*.exe | ForEach-Object -Process {
     Write-Host "Running " $_.Fullname
-    $proc = Start-Process $_.Fullname -ArgumentList "-t" -PassThru
+    $proc = Start-Process -FilePath $_.Fullname -ArgumentList "-tf" -PassThru
     Write-Host "  started..."
     Wait-Process -InputObject $proc
     Write-Host "  complete"
